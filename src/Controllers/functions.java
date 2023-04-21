76a946de3aec0077b341dec78ccac4dcce5ebc1a
mxttw <matthew.a.c.04@gmail.com>
2023-04-21 09:03:57 +0200

    update

--- src/Controllers/functions.java
+++ src/Controllers/functions.java
@@ -138,6 +138,7 @@ public class functions {
             for (Animals objeto : cincoObjetos) {
                 System.out.println(objeto);
             }
+            
 
             //TODO: Cojer caballo de tu lista
             StartRace();
@@ -148,7 +149,7 @@ public class functions {
     public void StartRace() {
         //TODO: EmpezarCarrera
         Animals[][] RaceBoard = new Animals[5][8];
-        
+
         for (int i = 0; i < cincoObjetos.size(); i++) {
             RaceBoard[i][0] = cincoObjetos.get(i);
         }
